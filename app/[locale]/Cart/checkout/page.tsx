1f5e1b98b33110a5a86fd9c0f644baee5490b468
Mohamed Habib <byaalkhy43@gmail.com>
2025-06-29 00:01:05 +0300

    refactor: update Checkout page layout and add CheckoutForms component

--- app/[locale]/Cart/checkout/page.tsx
+++ app/[locale]/Cart/checkout/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+import CheckoutForms from "../../components/CheckoutForms"
 
 
 
@@ -5,7 +8,20 @@
 export default function Checkout(params: []) {
     return (
         <section>
-            <h1>Welcome To Cehckout Page</h1>
+        <div className="custom__container">
+        <div className='flex items-center gap-2 mb-8 text-[#393939] lg:text-lg mr-auto text-sm'>
+         <Link  href={"/"} className='text-[#868686]'>Home</Link>
+         <Image
+         src={"/arrow-right.svg"}
+         alt="Arrow Right"
+         width={24}
+         height={24}
+         />
+           <div className='font-semibold'>Checkout</div>
+         </div>
+         <h3 className="text-[#393939] font-semibold text-3xl mb-3.5 pb-3.5">Checkout</h3>
+         <CheckoutForms />
+        </div>
         </section>
     )
 }
